4a913558e6b2dfd8c86ac0c0d4c1b68fd7b25696
taleore <taleore@gmail.com>
2019-07-29 16:19:02 +0200

    Add Js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const userNumber = document.querySelector(".main__number");
+const button = document.querySelector(".main__submit__btn");
+const myRandomNumber = getRandomNumber(100);
+const resultNumber = document.querySelector(".main__result");
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+console.log(`Mi número aleatorio es ${myRandomNumber}`);
+
+function compareNumbers() {
+  const userNumberValue = parseInt(userNumber.value);
+
+  if (userNumberValue === myRandomNumber) {
+    resultNumber.innerHTML = "HAS GANADO, CAMPEONA!";
+  } else if (userNumberValue < myRandomNumber){
+    resultNumber.innerHTML = "Demasiado bajo";
+  } else () {
+    resultNumber.innerHTML = "Demasiado alto";
+  }
+}
+
+//function showResult() {}
+//button.addEventListener('click', showResult());
